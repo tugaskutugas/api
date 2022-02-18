fc8fd0cdf57579c0749c72ce51a9f86c83c6ed15
tugaskutugas <99940873+tugaskutugas@users.noreply.github.com>
2022-02-18 11:56:07 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+console.log('Memulai server...');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+const init = async () => {
+ const server = Hapi.server({
+ port: 5000,
+ host: 'localhost',
+ routes: {
+ cors: {
+ origin: ['*'],
+ },
+ },
+ });
+ server.route(routes);
+ await server.start();
+ console.log(`Server sedang berjalan di ${server.info.uri}`);
+};
+init();
